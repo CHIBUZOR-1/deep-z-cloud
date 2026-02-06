08d9bd90a186ea14b573f49536314537a60a1a03
Chibuzor Amaechi <amaechihenrychibuzor@gmail.com>
2026-02-06 14:29:52 +0100

    cors and preflight fix2

--- Server/index.js
+++ Server/index.js
@@ -85,5 +85,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening @ http://localhost:${PORT}`);
+    console.log(`Server listening at http://localhost:${PORT}`);
 });
